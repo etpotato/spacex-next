01554ea200f835c78acb5fa223f2313253c8d3ca
etpotato <mareev.pv@gmail.com>
2022-05-24 14:54:14 +0300

    Add unique launches filter

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage, GetStaticProps } from 'next'
 import type { ApolloQueryResult } from '@apollo/client'
 
 import { LaunchesDocument, LaunchesQuery } from '../libs/graphql/__generated__'
+import getUniqueLaunches from '../libs/launches/getUniqueLaunches'
 import client from '../libs/apollo-client'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -9,16 +10,20 @@ import Rocket from '../components/Icons/Rocket'
 import Clock from '../components/Icons/Clock'
 import Carousel from '../components/Carousel/Carousel'
 import Section from '../components/Section/Section'
+
 interface props {
   response: ApolloQueryResult<LaunchesQuery>,
   error: { message: String, exist: Boolean },
 }
 
 const Main: NextPage<props> = ({ response, error }) => {
-  const list = response?.data?.launches?.map(launch => {
-    const date = launch?.launch_date_local
-      && new Date(launch?.launch_date_local).toUTCString()
-  
+  const list = getUniqueLaunches(response)?.map(launch => {
+    const date = launch?.launch_date_utc
+      && new Date(launch?.launch_date_utc).toUTCString()
+    const imageSrc = launch?.links?.mission_patch_small;
+
+    console.log(response);
+
     return ({
       key: launch?.id,
       element: (
@@ -34,7 +39,7 @@ const Main: NextPage<props> = ({ response, error }) => {
               <span className='col-start-2'>{launch?.rocket?.rocket_name}</span>
             </p>
             <div className='relative w-16 h-16 col-start-3 row-start-1 row-end-2'>
-              <Image src={launch?.links?.mission_patch_small || ''} alt='mission patch' layout='fill'/>
+              { imageSrc ? <Image src={imageSrc} alt='mission patch' layout='fill'/> : null }
             </div>
           </a>
         </Link>
@@ -54,9 +59,7 @@ export const getStaticProps: GetStaticProps = async () => {
     const response = await client.query({
       query: LaunchesDocument,
       variables: {
-        limit: 12,
-        sort: 'launch_year',
-        order: 'desc',
+        limit: 6,
       },
     })
 
