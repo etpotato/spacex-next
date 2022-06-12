fe9ec284791ee41e12761173e232b8f2102a43bf
etpotato <mareev.pv@gmail.com>
2022-06-12 17:22:54 +0300

    Add footer

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -13,6 +13,17 @@ const Layout: React.FC<props> = ({ children }) => {
           { children }
         </div>
       </main>
+      <footer className='pt-12 pb-4 sm:pb-8'>
+        <div className='xl:container grid gap-3 items-center justify-center sm:grid-cols-3 w-full mx-auto px-8 md:px-12 lg:px-16 min-h-full'>
+          <a className='max-w-max sm:col-start-3 sm:justify-self-end underline underline-offset-2 hover:-translate-y-1 focus:-translate-y-1 hover:text-cyan-400 focus:text-cyan-400'
+            href='http://pavelmareev.dev/'
+            target='_blank'
+            rel='noreferrer'>
+            pavelmareev.dev
+          </a>
+          <p className='text-center sm:col-start-2 sm:row-start-1'>&copy;2022</p>
+        </div>
+      </footer>
     </div>
   )
 }
